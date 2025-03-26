241740ed56fda97d0476ddf253b05c2a4bfd42be
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2025-03-26 13:19:19 +0200

    feat: implement i18n support for import confirmation dialog and messages in CSV import functionality

--- custom/ImportCsv.vue
+++ custom/ImportCsv.vue
@@ -6,19 +6,19 @@
         aria-hidden="true" class="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
     </div>
   <Dialog ref="confirmDialog" 
-    header="Import Confirmation"
+    :header="t('Import Confirmation')"
     :buttons="[
-      { label: 'Import & Replace', onclick: (dialog) => { confirmImport(dialog) } },
-      { label: 'Import New Only', onclick: (dialog) => { confirmImportNewOnly(dialog) } },
-      { label: 'Cancel', onclick: (dialog) => dialog.hide() }
+      { label: t('Import & Replace'), onclick: (dialog) => { confirmImport(dialog) } },
+      { label: t('Import New Only'), onclick: (dialog) => { confirmImportNewOnly(dialog) } },
+      { label: t('Cancel'), onclick: (dialog) => dialog.hide() }
     ]"
   >
     <div v-if="importStats">
-      <p>Are you sure you want to continue?</p>
-      <p class="mt-2">You are importing {{ importStats.total }} records:</p>
+      <p>{{ $t('Are you sure you want to continue?') }}</p>
+      <p class="mt-2">{{ $t('You are importing {count} records:', { count: importStats.total }) }}</p>
       <ul class="list-disc ml-6 mt-2">
-        <li>{{ importStats.existingCount }} existing records will be replaced</li>
-        <li>{{ importStats.newCount }} new records will be added</li>
+        <li>{{ $t('{count} existing records will be replaced (old records will be lost)', { count: importStats.existingCount }) }}</li>
+        <li>{{ $t('{count} new records will be added', { count: importStats.newCount }) }}</li>
       </ul>
     </div>
   </Dialog>
@@ -30,6 +30,9 @@ import { callAdminForthApi } from '@/utils';
 import adminforth from '@/adminforth';
 import Papa from 'papaparse';
 import { Dialog } from '@/afcl';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const inProgress: Ref<boolean> = ref(false);
 const confirmDialog = ref(null);
